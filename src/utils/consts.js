8f44929a516daacede072af344d3e3a714fed59a
Gelan007 <anatoliy.fil033@gmail.com>
2022-06-11 19:15:24 +0300

    Улучшенный Navbar, а именно сделаны нормальные правильные ссылки через константы, немного отформатировано, теперь показывает в зависимости от того зарегестрирован или нет

--- src/utils/consts.js
+++ src/utils/consts.js
@@ -4,3 +4,4 @@ export const REGISTRATION_ROUTE = "/registration";
 export const HOME_ROUTE = "/";
 export const QUESTS_ROUTE = "/quests";
 export const ACCOUNT_ROUTE = "/account";
+export const RIGHTS_ROUTE = "/rights";
